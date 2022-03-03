2569136cd75d1c63816d99373201204bd45a83c8
Gs-1998 <s1081646@studenti.univpm.it>
2022-03-03 20:04:34 +0100

    aggiunta funzione suggerimento_indirizzo_utente

--- funzioni.py
+++ funzioni.py
@@ -1,3 +1,5 @@
+from eth_account import Account
+import secrets
 
 # Validazione input: controlla la lunghezza e restituisce la stringa validata
 # Continua a chiedere l'input per 5 volte
@@ -16,3 +18,11 @@ def input_val(max_len = 43, max_retry = 5):
             c += 1
 
     return in_str
+
+
+def suggerimento_address_utente():
+    priv = secrets.token_hex(32)
+    private_key = "0x" + priv
+    acct = Account.from_key(private_key)
+    return private_key, acct.address
+
