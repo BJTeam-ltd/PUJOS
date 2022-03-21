416928049e6daa3ed1fb1bd2a84b4691e3664a0f
Francesco Traini <s1082066@studenti.univpm.it>
2022-03-21 18:35:55 +0100

    address relativo al nuovo smart contract

--- address.py
+++ address.py
@@ -1,4 +1,6 @@
 
-contract_address = "0xBca0fDc68d9b21b5bfB16D784389807017B2bbbc" #Indirizzo smart contract quando caricato
+contract_address = "0x37dBe0eFae1F30F43FdA9cD7dBCc0B364ED4F85f" #Indirizzo smart contract quando caricato
 
 admin_address = "0xC9C913c8c3C1Cd416d80A0abF475db2062F161f6" #Indirizzo admin
+
+null_address = "0x0000000000000000000000000000000000000000"
